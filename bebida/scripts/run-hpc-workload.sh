1a82dd59170c9a3da7e0cb940f55325a6473998c
Michael Mercier <michael.mercier@ryax.tech>
2024-03-26 11:58:09 +0100

    Fix scripts running demo

--- bebida/scripts/run-hpc-workload.sh
+++ bebida/scripts/run-hpc-workload.sh
@@ -6,10 +6,11 @@ set -u
 
 SIZE=$1
 RESULTS_DIR=$2
+SPARK_APP=${3:-$HOME/spark-pi.yaml}
 
 export ESPHOME=$(dirname $(dirname $(realpath $(which mkjobmix))))
 
-EXPE_DIR=expe-$(date --iso-8601=hours | tr ':' '-' | tr '+' '-')
+EXPE_DIR=expe-$(date --iso-8601=minutes | tr ':' '-' | tr '+' '-')
 export ESPSCRATCH=$HOME/$EXPE_DIR
 mkdir -p $ESPSCRATCH/logs
 mkdir -p $ESPSCRATCH/jobmix
@@ -22,25 +23,35 @@ chmod +x ./*
 cd ..
 
 get_result() {
-  echo "\n== Copy results from $ESPSCRATCH to $RESULTS_DIR"
-  cp -r $ESPSCRATCH $RESULTS_DIR/$EXPE_DIR
-  echo == Get all history and logs
+  echo "=== Kill HPC workflow submission"
+  kill $PID
+  echo "\n=== Copy results from $ESPSCRATCH to $RESULTS_DIR"
+  mkdir $RESULTS_DIR/$EXPE_DIR
+  cp -r $ESPSCRATCH/* $RESULTS_DIR/$EXPE_DIR
+  echo === Get all history and logs
   # FIXME Should be enough for this year ^^
   oarstat --gantt "2024-01-01 00:00:00, 2025-01-01 00:00:00" -Jf > $RESULTS_DIR/$EXPE_DIR/oar-jobs.json
   k3s kubectl get events -o json > $RESULTS_DIR/$EXPE_DIR/k8s-events.json
+  # Copy this script
+  cp "${BASH_SOURCE[0]}" $RESULTS_DIR/$EXPE_DIR/expe-script.sh
   journalctl -u bebida-shaker.service > $RESULTS_DIR/$EXPE_DIR/shaker.log
+  echo === Experiment done! 
+  echo Results are located here:
+  echo $RESULTS_DIR/$EXPE_DIR
 }
 
 trap get_result EXIT
 
-kubectl apply -f /etc/demo/spark-setup.yaml
+k3s kubectl apply -f /etc/demo/spark-setup.yaml
 
 runesp -v -T 10 -b OAR &
+PID=$!
 
 for run in seq 5
 do
-    kubectl apply -f /etc/demo/spark-pi.yaml
-    kubectl wait --for=condition=Completed pod/spark-app-pi
+    k3s kubectl apply -f $SPARK_APP
+    k3s kubectl wait --for=jsonpath='{.status.phase}'=Succeeded pod/spark-app-pi --timeout=3600s
+    k3s kubectl delete -f $SPARK_APP
     sleep 5
 done
 echo Done!
