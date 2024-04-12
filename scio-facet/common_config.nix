212d0bb5a41d44d1b56501e8f4d8969dff96f9fb
Pedro Velho <pedro.velho@ryax.tech>
2024-04-12 11:20:58 +0200

    First version facet nxc.

---
+++ scio-facet/common_config.nix
@@ -0,0 +1,61 @@
+{ pkgs, modulesPath, HPCScheduler }:
+let
+  inherit (import "${toString modulesPath}/tests/ssh-keys.nix" pkgs) snakeOilPrivateKey snakeOilPublicKey;
+  sshPrivateKey = ./secrets/ssh_key;
+  sshPublicKey = ./secrets/ssh_key.pub;
+in
+{
+  # Inject key to permit localhost ssh from bebida-shaker
+  users.users.root.openssh.authorizedKeys.keys = [
+    (builtins.readFile sshPublicKey)
+  ];
+
+  environment.systemPackages = [
+    pkgs.python3
+    pkgs.emacs
+    pkgs.vim
+    pkgs.cpufrequtils
+    pkgs.python3Packages.clustershell
+    pkgs.htop
+    pkgs.tree
+    pkgs.cri-tools
+  ];
+
+  environment.noXlibs = false;
+
+  nxc.users = { names = [ "ryax" ]; prefixHome = "/users"; };
+
+  security.pam.loginLimits = [
+    { domain = "*"; item = "memlock"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "stack"; type = "-"; value = "unlimited"; }
+  ];
+
+  system.activationScripts.ryax-user-init = ''
+    mkdir -p /users/ryax/.ssh
+    echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDX8e0uKbhUu6MWGnbMzq+kdGOVWSn0mPeD0VeQxn5QbWdmUGVyRe8IoNlM1TpfFHA0ZdnYNTT5c4CT+qyPikMVr1IUlH0Rup2/ieIWA8ljUwRX9o3Kw1GN0xc+mQxr3AAAjA8CwHxFX2bih/xacQz85hQguUofRdzlpfJ1fZoB+Dw/Rpd4aUiSNNGt+NYmKrVRvMJhFsQkQm+TzI6P8WP6GPegdS2jINUzwUC0jEWHQz5fs5c6jVDOPBXE8/qHusu1SHC6FlhZjyOXCmMYAPn9v11iLCn3Z2C2wufBU9sHztHHYU2p/a4v23LN+Esn+l9+gYwUQ68t9oIBmG8K2tu9MwGNlCMziJRx6G2JHiQNoA5Gu5zTbJVLEYFF00upoFGEDNObLzt0gQugkiTd4kvyC72Up0DM+qnVzIfhnDg7uddpYxmal0koOwmeuqyYUz75Ti+hOZ8VLkJ90C+KD8UzZp7O7++5hefXHYpx89fgs4tbv5W0KOUnxah6rX4T1ms= slurmuser@ryax" >> /users/ryax/.ssh/authorized_keys
+    cat > /users/ryax/requirements.txt <<EOF
+    pyspark==3.2.1
+    numpy==1.22.3
+    dbfread==2.0.7
+    pandas==1.3.4
+    datar==0.8.1
+    tifffile==2022.5.4
+    pyarrow==10.0.0
+    EOF
+    pip -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+  '';
+
+
+
+  environment.etc."privkey.snakeoil" = {
+    mode = "0600";
+    source = snakeOilPrivateKey;
+  };
+
+  environment.etc."pubkey.snakeoil" = {
+    mode = "0600";
+    text = snakeOilPublicKey;
+  };
+}
---
+++ scio-facet/slurm_config.nix
@@ -0,0 +1,55 @@
+{ pkgs, nbNodes }:
+let
+  passFile = pkgs.writeText "dbdpassword" "password123";
+in
+{
+  services.slurm = {
+    controlMachine = "server";
+    nodeName = [ "node[1-${nbNodes}] CPUs=1 State=UNKNOWN" ];
+    partitionName = [
+      "DEFAULT Nodes=node[1-${nbNodes}] Default=YES State=UP DefaultTime=60"
+      "bebida Nodes=node[1-${nbNodes}] Default=YES MaxTime=INFINITE"
+    ];
+  };
+
+  # Avoid error about xauth missing...
+  services.openssh.settings.X11Forwarding = false;
+  services.slurm.dbdserver = {
+    dbdHost = "server";
+    storagePassFile = "${passFile}";
+  };
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+    initialScript = pkgs.writeText "mysql-init.sql" ''
+      CREATE USER 'slurm'@'localhost' IDENTIFIED BY 'password123';
+      GRANT ALL PRIVILEGES ON slurm_acct_db.* TO 'slurm'@'localhost';
+    '';
+    ensureDatabases = [ "slurm_acct_db" ];
+    ensureUsers = [{
+      ensurePermissions = { "slurm_acct_db.*" = "ALL PRIVILEGES"; };
+      name = "slurm";
+    }];
+    settings.mysqld = {
+      # recommendations from: https://slurm.schedmd.com/accounting.html#mysql-configuration
+      innodb_buffer_pool_size = "1024M";
+      innodb_log_file_size = "64M";
+      innodb_lock_wait_timeout = 900;
+    };
+  };
+
+  systemd.services.slurmdbd.serviceConfig = {
+    Restart = "on-failure";
+    RestartSec = 3;
+  };
+  systemd.services.slurmctld.serviceConfig = {
+    Restart = "on-failure";
+    RestartSec = 3;
+  };
+
+  systemd.tmpfiles.rules = [
+    "f /etc/munge/munge.key 0400 munge munge - mungeverryweakkeybuteasytointegratoinatest"
+  ];
+  networking.firewall.enable = false;
+
+}
